2d6522422c7899c19b85b1239c4a34a0a86fdc5d
Chris Mears <chris.mears@gmail.com>
2019-10-28 19:12:00 -0700

    Initial commit from gatsby: (https://github.com/hasura/gatsby-gitbook-starter.git)

---
+++ config.js
@@ -0,0 +1,45 @@
+const config = {
+	"gatsby": {
+		"pathPrefix": "/",
+		"siteUrl": "https://learn.hasura.io",
+		"gaTrackingId": null
+	},
+	"header": {
+		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
+		"logoLink": "https://learn.hasura.io",
+		"title": "Gatsby Gitbook Boilerplate",
+		"githubUrl": "https://github.com/hasura/gatsby-gitbook-boilerplate",
+		"helpUrl": "",
+		"tweetText": "",
+		"links": [
+			{ "text": "", "link": ""}
+		],
+		"search": {
+			"enabled": false,
+			"indexName": "",
+			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
+			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+			"algoliaAdminKey": process.env.ALGOLIA_ADMIN_KEY
+		}
+	},
+	"sidebar": {
+		"forcedNavOrder": [
+			"/introduction",
+    		"/codeblock"
+		],
+		"links": [
+			{ "text": "Hasura", "link": "https://hasura.io"},
+		],
+		"frontline": false,
+		"ignoreIndex": true,
+	},
+	"siteMetadata": {
+		"title": "Gatsby Gitbook Boilerplate | Hasura",
+		"description": "Documentation built with mdx. Powering learn.hasura.io ",
+		"ogImage": null,
+		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
+		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
+	},
+};
+
+module.exports = config;
